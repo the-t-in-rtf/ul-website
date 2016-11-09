6e9fdee2e14a3722c8887d1a7607b991ef3c78a9
Tony Atkins <tony@raisingthefloor.org>
2016-11-09 12:08:43 +0100

    UL-66:  Hid unused “cancel” button on “suggest” form.

--- src/js/client/suggest.js
+++ src/js/client/suggest.js
@@ -6,18 +6,32 @@
 /* global fluid */
 (function () {
     "use strict";
+    var gpii = fluid.registerNamespace("gpii");
+
+    fluid.registerNamespace("gpii.ul.website.suggest.client");
+    gpii.ul.website.suggest.client.hideCancelButton = function (that) {
+        var cancelButton = that.locate("cancelButton");
+        cancelButton.hide();
+    };
 
     // The "suggest" form is only for editing a suggestion, and is created with the correct data..
     fluid.defaults("gpii.ul.website.suggest.client", {
         gradeNames: ["gpii.ul.product.edit"],
         selectors: {
-            viewport: ".suggest-viewport"
+            viewport:     ".suggest-viewport",
+            cancelButton: ".cancel-button"
         },
         model: {
             successMessage:   false,
             errorMessage:     false,
             product:          false,
             user:             false
+        },
+        listeners: {
+            "onDomChange.hideCancelButton": {
+                funcName: "gpii.ul.website.suggest.client.hideCancelButton",
+                args:     ["{that}"]
+            }
         },
         invokers: {
             renderInitialMarkup: {
