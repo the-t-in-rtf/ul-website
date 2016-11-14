1ba1a46cf1854a248fd4df20921a62f2118c0d21
Tony Atkins <tony@raisingthefloor.org>
2016-11-14 10:55:53 +0100

    UL-66: Added “rollup” file to run all tests.

---
+++ tests/all-tests.js
@@ -0,0 +1,16 @@
+/* eslint-env node */
+"use strict";
+
+require("./js/browser-404-tests");
+
+// TODO:  Enable these once one or both of the blocking bugs are fixed.
+// require("./js/browser-checkboxPanel-tests");
+
+require("./js/browser-404-tests");
+require("./js/browser-cors-tests");
+require("./js/browser-docs-tests");
+require("./js/browser-login-tests");
+require("./js/browser-products-tests");
+require("./js/browser-search-tests");
+require("./js/browser-skipnav-tests");
+
